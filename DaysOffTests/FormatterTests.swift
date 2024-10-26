062345ea732c8cc86ce1c4afd8c52e39b95bfb8b
Tony <tony.short2@gmail.com>
2024-10-26 17:10:44 +0100

    Adds missing test for 1 dp formatter

--- DaysOffTests/FormatterTests.swift
+++ DaysOffTests/FormatterTests.swift
@@ -17,4 +17,10 @@ struct FormatterTests {
         let date = Calendar.current.date(from: dc)!
         #expect(Formatters.dateFormatter.string(for: date) == "Mon 1st Jan 24")
     }
+
+    @Test func oneDPFormat() async {
+        #expect(Formatters.oneDPFormat.format(1.0) == "1")
+        #expect(Formatters.oneDPFormat.format(1.5) == "1.5")
+    }
+
 }
